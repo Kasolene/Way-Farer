583c4204a60a673fdb70fddd963ee0103b6e7a5a
unknown <[nicolaskasolene@gmail.com]>
2019-08-03 17:49:09 +0200

    ft(signup): add a signup model  file  [#167688267]

---
+++ server/models/Users.js
@@ -0,0 +1,36 @@
+const users = [
+  {
+    id: 1,
+    email: 'nico@gmail.com',
+    firstName: 'nicolas',
+    lastName: 'kasolene',
+    password: 'nico123',
+    isAdmin: true,
+  },
+
+  {
+    id: 2,
+    email: 'nicolas@gmail.com',
+    firstName: 'nicolas',
+    lastName: 'kasolene',
+    password: 'nicolas123',
+    isAdmin: false,
+  },
+  {
+    id: 3,
+    email: 'nickson@gmail.com',
+    firstName: 'nickson',
+    lastName: 'kasolene',
+    password: 'nickson123',
+    isAdmin: false,
+  },
+  {
+    id: 4,
+    email: 'nickas@gmail.com',
+    firstName: 'nick',
+    lastName: 'kasolene',
+    password: 'nico123',
+    isAdmin: false,
+  },
+];
+export default users;
