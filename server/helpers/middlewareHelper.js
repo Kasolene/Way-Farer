9aef825e565c192cb81d01cde43c2b4231dfa4a0
unknown <[nicolaskasolene@gmail.com]>
2019-08-04 22:37:19 +0200

    add a helper file

---
+++ server/helpers/middlewareHelper.js
@@ -0,0 +1,19 @@
+export function validateToken(token) {
+  if (token && token.startsWith('Bearer ')) {
+    return token.slice(7, token.length);
+  }
+  return token;
+}
+
+export function tokenError(res) {
+  return res.status(401).json({
+    status: 401,
+    message: 'The authorization token provided is invalid',
+  });
+}
+export function TokenUnauthorized(res) {
+  return res.status(403).json({
+    status: 403,
+    message: 'You are not authorized to access this resource',
+  });
+}
