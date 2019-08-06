1bcd804d5617ebc198dc589d4872bf3c7dfafec9
unknown <[nicolaskasolene@gmail.com]>
2019-08-06 15:28:04 +0200

    updates on signin endpoints

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,41 +1,52 @@
 import {hashPassword} from '../helpers/hashPassword';
 const users = [
   {
-    user_id: 1,
-    email: 'nico@gmail.com',
-    first_name: 'nicolas',
-    last_name: 'kasolene',
-    password: hashPassword('nico123'),
-    is_admin: true,
-    token:0,
+    status: 200,
+    data : {
+      token:0,
+      user_id: 1,
+      email: 'nico@gmail.com',
+      first_name: 'nicolas',
+      last_name: 'kasolene',
+      password: hashPassword('nico123'),
+      is_admin: true,
+  }
   },
 
   {
-    user_id: 2,
-    email: 'nicolas@gmail.com',
-    first_name: 'nicolas',
-    last_name: 'kasolene',
-    password: hashPassword('nicolas123'),
-    is_admin: false,
-    token:0,
+    status : 200,
+    data : {
+      token:0,
+      user_id: 2,
+      email: 'nicolas@gmail.com',
+      first_name: 'nicolas',
+      last_name: 'kasolene',
+      password: hashPassword('nicolas123'),
+      is_admin: false,
+    }
   },
   {
-    user_id: 3,
-    email: 'nickson@gmail.com',
-    first_name: 'nickson',
-    last_name: 'kasolene',
-    password: hashPassword('nickson123'),
-    is_admin: false,
-    token:0,
+    status : 200,
+    data : {
+      token:0,
+      user_id: 3,
+      email: 'nickson@gmail.com',
+      first_name: 'nickson',
+      last_name: 'kasolene',
+      password: hashPassword('nickson123'),
+      is_admin: false,
+    }
   },
   {
-    user_id: 4,
-    email: 'nickas@gmail.com',
-    first_name: 'nick',
-    last_name: 'kasolene',
-    password: hashPassword('nico123'),
-    is_admin: true,
-    token:0,
+    status : 200,
+    data : {
+      token:0,
+      user_id: 4,
+      email: 'nickas@gmail.com',
+      first_name: 'nick',
+      last_name: 'kasolene',
+      password: hashPassword('nico123'),
+      is_admin: true,
+    }
   },
 ];
-export default users;
