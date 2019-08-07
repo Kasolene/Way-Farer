f87cc56f40c4ceb681101e429c6a3f46e474da34
unknown <[nicolaskasolene@gmail.com]>
2019-08-07 17:19:40 +0200

    updates

--- server/controllers/tripController.js
+++ server/controllers/tripController.js
@@ -1,11 +1,17 @@
 import trips from '../models/Trips';
 
+/**
+ * Adds two numbers together.
+ * @param req The first number.
+ * @param res num2 The second number.
+ * @returns {int} The sum of the two numbers.
+ */
 export function createAtrip(req, res) {
   const {
     busLicenseNumber, seatingCapacity, origin, destination, fare, status,
   } = req.body;
   trips.push({
-    tripId: trips.length,
+    tripId: trips.length, 
     busLicenseNumber,
     seatingCapacity,
     origin,
